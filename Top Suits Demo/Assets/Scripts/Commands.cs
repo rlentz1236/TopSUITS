417c79d5369f51a180551562322ddbb8d0fb5765
rlentz1236 <rlentz1236@gmail.com>
2018-03-10 10:46:24 -0500

    demo 2

---
+++ Top Suits Demo/Assets/Scripts/Commands.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Commands : MonoBehaviour {
+
+    public GameObject uiDiagram;
+    public GameObject uiProcedure;
+    public GameObject uiProcedrueTitle;
+    public GameObject uiProcedureStepTitle;
+    public GameObject uiProcedureStepDescript;
+    private ProcedureObject procedureObject;
+    private Text txt;
+
+    // Use this for initialization
+    void Start () {
+
+        uiDiagram.SetActive(true);
+        uiProcedure.SetActive(true);
+    }
+
+    void OnHideProcedure()
+    {
+        Debug.Log("Entered On Hide Procedure");
+        uiProcedure.SetActive(false);
+    }
+
+    void OnShowProcedure()
+    {
+        Debug.Log("Entered On Show Procedure");
+        uiProcedure.SetActive(true);
+    }
+
+    void OnShowDiagram()
+    {
+        Debug.Log("Entered On Show Diagram");
+        uiDiagram.SetActive(true);
+    }
+
+    void OnHideDiagram()
+    {
+        Debug.Log("Entered On Hide Diagram");
+        uiDiagram.SetActive(false);
+    }
+
+    void OnStartDisableAlarm()
+    {
+        Debug.Log("Entered On Start Disbale Alarm");
+        uiProcedure.SetActive(true);
+
+        procedureObject = new ProcedureObject();
+        txt = uiProcedrueTitle.GetComponent<Text>();
+        txt.text = procedureObject.GetTitle();
+
+        string[] temp;
+
+        temp = procedureObject.OnNextStep();
+
+        txt = uiProcedureStepTitle.GetComponent<Text>();
+        txt.text = temp[0];
+
+        txt = uiProcedureStepDescript.GetComponent<Text>();
+        txt.text = temp[1] + "\n" + temp[2];
+    }
+
+    void OnNextStep()
+    {
+        string[] temp;
+
+        temp = procedureObject.OnNextStep();
+
+        txt = uiProcedureStepTitle.GetComponent<Text>();
+        txt.text = temp[0];
+
+        txt = uiProcedureStepDescript.GetComponent<Text>();
+        txt.text = temp[1] + "\n" + temp[2];
+    }
+
+    void OnPreviousStep()
+    {
+        string[] temp;
+
+        temp = procedureObject.OnPreviousStep();
+
+        txt = uiProcedureStepTitle.GetComponent<Text>();
+        txt.text = temp[0];
+
+        txt = uiProcedureStepDescript.GetComponent<Text>();
+        txt.text = temp[1] + "\n" + temp[2];
+    }
+
+}
---
+++ Top Suits Demo/Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("speech manager on start entered");
+
+        /*keywords.Add("", () =>
+        {
+            Debug.Log("");
+            this.BroadcastMessage("");
+        });*/
+
+        keywords.Add("Show Diagram", () =>
+        {
+            Debug.Log("entered show diagram keyword");
+            this.BroadcastMessage("OnShowDiagram");
+        });
+
+        keywords.Add("Hide Diagram", () =>
+        {
+            Debug.Log("entered hide diagram keyword");
+            this.BroadcastMessage("OnHideDiagram");
+        });
+
+        keywords.Add("Show Procedure", () =>
+        {
+            Debug.Log("entered show diagram keyword");
+            this.BroadcastMessage("OnShowProcedure");
+        });
+
+        keywords.Add("Hide Procedure", () =>
+        {
+            Debug.Log("entered hide diagram keyword");
+            this.BroadcastMessage("OnHideProcedure");
+        });
+
+        keywords.Add("Start Disable Alarm Procedure", () =>
+        {
+            Debug.Log("entered start disable alarm procedure keyword");
+            this.BroadcastMessage("OnStartDisableAlarm");
+        });
+
+        keywords.Add("Next Step", () =>
+        {
+            Debug.Log("entered next step keyword");
+            this.BroadcastMessage("OnNextStep");
+        });
+
+        keywords.Add("Previous Step", () =>
+        {
+            Debug.Log("entered previous step keyword");
+            this.BroadcastMessage("OnPreviousStep");
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
